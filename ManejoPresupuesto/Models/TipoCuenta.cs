fc71382768623fa6a71e2ae71c64353114bce730
Jimmy Grados <died_sistemas25@minedu.gob.pe>
2024-05-03 19:21:09 -0500

    Add project files.

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,49 @@
+﻿using ManejoPresupuesto.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta //: IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [FirstLetterCapitalized]
+        //[StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        //[Display(Name = "Nombre del tipo de cuenta")]
+        public string Nombre { get; set; }
+        
+        public int UsuarioId { get; set; }
+        
+        public int Orden { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0)
+        //    {
+        //        var firstLetter = Nombre[0].ToString();
+
+        //        if (firstLetter != firstLetter.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe ser mayúscula", new[] {nameof(Nombre)});
+        //        }
+        //    }
+        //}
+
+        /*Pruebas de otras validaciones por defecto*/
+
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[EmailAddress(ErrorMessage = "El campo debe ser un email válido")]
+        //public string Email { get; set; }
+
+        //[Range(minimum: 18, maximum: 130, ErrorMessage = "El valor debe estar entre {1} y {2}")]
+        //public int Edad { get; set; }
+
+        //[Url(ErrorMessage = "El campo debe ser una URL válida")]
+        //public string URL { get; set; }
+
+        //[CreditCard(ErrorMessage = "La tarjeta de crédito no es válida")]
+        //[Display(Name = "Tarjeta de Crédito")]
+        //public string TarjetaDeCredito { get; set; }
+    }
+}
---
+++ ManejoPresupuesto/Validations/FirstLetterCapitalizedAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Validations
+{
+    public class FirstLetterCapitalizedAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var firstLetter = value.ToString()[0].ToString();
+            if (firstLetter != firstLetter.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayúsucla");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
