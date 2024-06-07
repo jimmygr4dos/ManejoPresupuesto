eba69c1a97d16d2844aece82d475f233740263f9
Jimmy Grados <died_sistemas25@minedu.gob.pe>
2024-06-07 00:48:11 -0500

    Prueba DevOps

--- DB/dbo/Tables/TiposOperaciones.sql
+++ DB/dbo/Tables/TiposOperaciones.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[TiposOperaciones] (
     [Id]          INT           IDENTITY (1, 1) NOT NULL,
     [Descripcion] NVARCHAR (50) NOT NULL,
+    [Prueba]      INT           NULL,
     CONSTRAINT [PK_TiposOperaciones] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
